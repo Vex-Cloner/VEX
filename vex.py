622d102ab351516a83894ceafcc064de61b01d37
Vex-Cloner <npanimexyt@gmail.com>
2024-07-26 01:58:44 +0545

    Update vex.py

--- vex.py
+++ vex.py
@@ -5,9 +5,9 @@ import sys
 
 is_64bits = sys.maxsize > 2**32
 os.system('clear')
-if is_64bits:
-    os.system("python vex64.py")
-elif is_32bits:
+if is_32bits:
     os.system("python vex32.py")
+elif is_64bits:
+    os.system("python vex64.py")
 else:
     print("unknown bit please try again later...")
