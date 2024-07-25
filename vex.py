617348b1bcb44903aa10888a99cf015bd3abb9e5
Vex-Cloner <npanimexyt@gmail.com>
2024-07-26 01:43:02 +0545

    Update vex.py

--- vex.py
+++ vex.py
@@ -3,12 +3,10 @@ import vex32
 import os
 import sys
 
-# Check if the platform is 64-bit
 is_64bits = sys.maxsize > 2**32
 import os, platform, time, sys
 os.system('clear')
 if is_64bits:
     vex64.menu()
-    # Perform 64-bit specific operations or imports here
 else:
     vex32.menu()
