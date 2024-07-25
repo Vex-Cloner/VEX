2a6aed19ed2c8008d2ca00b297d50cd9875cd2d1
Vex-Cloner <npanimexyt@gmail.com>
2024-07-26 02:03:28 +0545

    Update vex.py

--- vex.py
+++ vex.py
@@ -1,14 +1,2 @@
-import vex64
 import vex32
-import os
-import sys
-
-is_32bits = sys.maxsize
-is_64bits = sys.maxsize > 2**32
-os.system('clear')
-if is_32bits:
-    os.system("python vex32.py")
-elif is_64bits:
-    os.system("python vex64.py")
-else:
-    print("unknown bit please try again later...")
+vex32.menu()
