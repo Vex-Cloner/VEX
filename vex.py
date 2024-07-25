6833fd93c26ed3dd433ae11406f709dc2d75dbb5
Vex-Cloner <npanimexyt@gmail.com>
2024-07-26 01:42:08 +0545

    Update vex.py

--- vex.py
+++ vex.py
@@ -1,2 +1,14 @@
-import editv2
-editv2.menu()
+import vex64
+import vex32
+import os
+import sys
+
+# Check if the platform is 64-bit
+is_64bits = sys.maxsize > 2**32
+import os, platform, time, sys
+os.system('clear')
+if is_64bits:
+    vex64.menu()
+    # Perform 64-bit specific operations or imports here
+else:
+    vex32.menu()
